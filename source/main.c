e69777f2088fbb1b3ed57c779aac9c1fbfc9e251
shinyquagsire23 <mtinc2@gmail.com>
2015-10-17 22:23:12 -0700

    Cleanup, added aliases and worked around a UV bug in sf2dlib

--- source/main.c
+++ source/main.c
@@ -54,6 +54,7 @@ void printfile(const char* path)
 void *clim_tex_xy7(u8 *decomp_out, u32 decomp_size, u16 *out_data);
 sf2d_texture *create_texture_from_clim(u8 *decomp_out, u32 decomp_size);
 sf2d_texture *create_texture_from_xy7_clim(u8 *decomp_out, u32 decomp_size);
+sf2d_texture *load_texture_from_darc(void *darc_data, char *path);
 
 int main()
 {
@@ -117,19 +118,14 @@ int main()
 	    
 	if(darc_decomp)
 	{
-	    box_clim = dfget(pokedarc_get_darc(darc_decomp), "cursol01.bclim", &box_clim_size);
-	    box_bg_clim = dfget(pokedarc_get_darc(darc_decomp), "box_wp01.bclim", &box_bg_size);
-	    box_name = dfget(pokedarc_get_darc(darc_decomp), "box_name01.bclim", &box_name_size);
 	    printf("darc_file: %x\n", box_clim);
 	}
 	
 	printf("All read!");
-	sf2d_texture *tex1 = create_texture_from_clim(box_clim, box_clim_size);
-	sf2d_texture *tex2 = create_texture_from_xy7_clim(decomp_out, decomp_size);
-	sf2d_texture *tex3 = create_texture_from_clim(box_bg_clim, box_bg_size);
-	sf2d_texture *tex4 = create_texture_from_clim(box_name, box_name_size);
-	//clim_tex_xy7(box_clim, box_clim_size, tex1->data);
-	tex1->tiled = 1;
+	sf2d_texture *cursor = load_texture_from_darc(pokedarc_get_darc(darc_decomp), "cursol02.bclim");
+	sf2d_texture *mon = create_texture_from_xy7_clim(decomp_out, decomp_size);
+	sf2d_texture *wallpaper = load_texture_from_darc(pokedarc_get_darc(darc_decomp), "box_wp02.bclim");
+	sf2d_texture *name_box = load_texture_from_darc(pokedarc_get_darc(darc_decomp), "box_name02.bclim");
 
 	// Main loop
 	while (aptMainLoop())
@@ -137,10 +133,10 @@ int main()
 		hidScanInput();
 		
 		sf2d_start_frame(GFX_BOTTOM, GFX_LEFT);
-		    sf2d_draw_texture(tex3, 0, 0);
-		    sf2d_draw_texture(tex4, 20, 20);
-			sf2d_draw_texture(tex2, 100, 100);
-			sf2d_draw_texture(tex1, 100, 90);
+		    draw_texture(wallpaper, 0, 0);
+		    draw_texture(name_box, 20, 20);
+			draw_texture(mon, 100, 100);
+			draw_texture(cursor, 120, 80);
 		sf2d_end_frame();
 		
 		//if(decomp_out)
@@ -160,13 +156,26 @@ int main()
 	return 0;
 }
 
+void draw_texture(const sf2d_texture *texture, int x, int y)
+{
+    sf2d_draw_texture_part(texture, x, y, 0, next_pow2(texture->height) - texture->height, texture->width, texture->height);
+}
+
+sf2d_texture *load_texture_from_darc(void *darc_data, char *path)
+{
+    u32 size;
+    void *data;
+    data = dfget(darc_data, path, &size);
+    return create_texture_from_clim(data, size);
+}
+
 sf2d_texture *create_texture_from_clim(u8 *decomp_out, u32 decomp_size)
 {
     clim_header *clim_h = decomp_out + (decomp_size - 0x28);
 	sf2d_texture *tex = sf2d_create_texture(clim_h->width, clim_h->height, clim_to_sf2d[clim_h->format], SF2D_PLACE_RAM);
 	memcpy(tex->data, decomp_out, clim_h->pixel_data_size);
 	
-	tex->flip_h = 1;
+	tex->flip_h = 0;
 	tex->flip_v = 1;
 	return tex;
 }
@@ -178,212 +187,19 @@ sf2d_texture *create_texture_from_xy7_clim(u8 *decomp_out, u32 decomp_size)
 	
 	u16 *palette = decomp_out+(sizeof(u16)*2);
 	u16 num_colors = *(u16*)(decomp_out + sizeof(u16));
-	u32 squared_width = (clim_h->pixel_data_size / 2) >> 5; //sqrt(pixel_data_size / bpp)
 	u8 *pixel_data = decomp_out + (sizeof(u16)*2) + (sizeof(u16) * num_colors);
 	
 	//Un-palette the data
 	int i;
-	for(i = 0; i < (squared_width * squared_width) / 2; i++)
+	for(i = 0; i < (next_pow2(clim_h->width) * next_pow2(clim_h->height)) / 2; i++)
 	{
 	    *(u16*)(tex->data + (i * sizeof(u16) * sizeof(u16))) = palette[(pixel_data[i] & 0xF0) >> 4];
 	    *(u16*)(tex->data + (i * sizeof(u16) * sizeof(u16)) + sizeof(u16)) = palette[pixel_data[i] & 0xF];
 	}
 	
-	tex->flip_h = 1;
+	tex->flip_h = 0;
 	tex->flip_v = 1;
 	return tex;
-}
-
-void clim_draw_xy7(u8 *decomp_out, u32 decomp_size, u32 x_pos, u32 y_pos)
-{
-    clim_header *clim_h = decomp_out + (decomp_size - 0x28);
-	u8 *screenBottom = gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL);
-	
-	u16 *palette = decomp_out+(sizeof(u16)*2);
-	u16 num_colors = *(u16*)(decomp_out + sizeof(u16));
-	u32 squared_width = (clim_h->pixel_data_size / 2) >> 5; //sqrt(pixel_data_size / bpp)
-	
-	u8 *pixel_data = decomp_out + (sizeof(u16)*2) + (sizeof(u16) * num_colors);
-	
-	int i, x, y, bx, by, mbx, mby, mmbx, mmby;
-		
-	//Draw out that ETC image... Not fun :/
-	for(i = 0, x = 0, y = 0, bx = 0, by = 0, mbx = 0, mby = 0, mmbx = 0, mmby = 0; i < (squared_width*squared_width)/2; i++)
-	{
-	    //if(((pixel_data[i] & 0xF0) >> 4) != 0)
-	        *(u16*)(screenBottom + ((bx + x + mbx + mmbx + x_pos) * sizeof(u16)) + ((by + y + mby + mmby + y_pos) * 240 * sizeof(u16))) = palette[((pixel_data[i] & 0xF0) >> 4)];
-	    x++;
-	    if(x >= 2)
-	    {
-	        x = 0;
-	        y++;
-	    }
-	    if(y >= 2)
-	    {
-	        mbx += 2;
-	        y = 0;
-	    }
-	    if(mbx >= 4)
-	    {
-	        mby += 2;
-	        mbx = 0;
-	    }
-	    if(mby >= 4)
-	    {
-	        mby = 0;
-	        mmbx += 4;
-	    }
-	    if(mmbx >= (clim_h->tile_width << 2))
-	    {
-	        mmby += 4;
-	        mmbx = 0;
-	    }
-	    if(mmby >= (clim_h->tile_height << 2))
-	    {
-	        mmby = 0;
-	        bx += (clim_h->tile_width << 2);
-	    }
-	    if(bx >= squared_width)
-	    {
-	        by += (clim_h->tile_height << 2);
-	        bx = 0;
-	    }
-	     
-	    //if(pixel_data[i] & 0xF != 0)
-	        *(u16*)(screenBottom + ((bx + x + mbx + mmbx + x_pos) * sizeof(u16)) + ((by + y + mby + mmby + y_pos) * 240 * sizeof(u16))) = palette[pixel_data[i] & 0xF];
-	    x++;
-	    if(x >= 2)
-	    {
-	        x = 0;
-	        y++;
-	    }
-	    if(y >= 2)
-	    {
-	        mbx += 2;
-	        y = 0;
-	    }
-	    if(mbx >= 4)
-	    {
-	        mby += 2;
-	        mbx = 0;
-	    }
-	    if(mby >= 4)
-	    {
-	        mby = 0;
-	        mmbx += 4;
-	    }
-	    if(mmbx >= (clim_h->tile_width << 2))
-	    {
-	        mmby += 4;
-	        mmbx = 0;
-	    }
-	    if(mmby >= (clim_h->tile_height << 2))
-	    {
-	        mmby = 0;
-	        bx += (clim_h->tile_width << 2);
-	    }
-	    if(bx >= squared_width)
-	    {
-	        by += (clim_h->tile_height << 2);
-	        bx = 0;
-	    }
-	}
-}
-
-void *clim_tex_xy7(u8 *decomp_out, u32 decomp_size, u16 *out_data)
-{
-    clim_header *clim_h = decomp_out + (decomp_size - 0x28);
-	
-	u16 *palette = decomp_out+(sizeof(u16)*2);
-	u16 num_colors = *(u16*)(decomp_out + sizeof(u16));
-	u32 squared_width = (clim_h->pixel_data_size / 2) >> 5; //sqrt(pixel_data_size / bpp)
-	
-	u8 *pixel_data = decomp_out + (sizeof(u16)*2) + (sizeof(u16) * num_colors);
-	
-	int i, x, y, bx, by, mbx, mby, mmbx, mmby;
-		
-	//Draw out that ETC image... Not fun :/
-	for(i = 0, x = 0, y = 0, bx = 0, by = 0, mbx = 0, mby = 0, mmbx = 0, mmby = 0; i < (squared_width*squared_width)/2; i++)
-	{
-	    //if(((pixel_data[i] & 0xF0) >> 4) != 0)
-	        *(u16*)(out_data + ((bx + x + mbx + mmbx) * sizeof(u16)) + ((by + y + mby + mmby) * squared_width * sizeof(u16))) = palette[((pixel_data[i] & 0xF0) >> 4)];
-	    x++;
-	    if(x >= 2)
-	    {
-	        x = 0;
-	        y++;
-	    }
-	    if(y >= 2)
-	    {
-	        mbx += 2;
-	        y = 0;
-	    }
-	    if(mbx >= 4)
-	    {
-	        mby += 2;
-	        mbx = 0;
-	    }
-	    if(mby >= 4)
-	    {
-	        mby = 0;
-	        mmbx += 4;
-	    }
-	    if(mmbx >= (clim_h->tile_width << 2))
-	    {
-	        mmby += 4;
-	        mmbx = 0;
-	    }
-	    if(mmby >= (clim_h->tile_height << 2))
-	    {
-	        mmby = 0;
-	        bx += (clim_h->tile_width << 2);
-	    }
-	    if(bx >= squared_width)
-	    {
-	        by += (clim_h->tile_height << 2);
-	        bx = 0;
-	    }
-	     
-	    //if(pixel_data[i] & 0xF != 0)
-	        *(u16*)(out_data + ((bx + x + mbx + mmbx) * sizeof(u16)) + ((by + y + mby + mmby) * squared_width * sizeof(u16))) = palette[pixel_data[i] & 0xF];
-	    x++;
-	    if(x >= 2)
-	    {
-	        x = 0;
-	        y++;
-	    }
-	    if(y >= 2)
-	    {
-	        mbx += 2;
-	        y = 0;
-	    }
-	    if(mbx >= 4)
-	    {
-	        mby += 2;
-	        mbx = 0;
-	    }
-	    if(mby >= 4)
-	    {
-	        mby = 0;
-	        mmbx += 4;
-	    }
-	    if(mmbx >= (clim_h->tile_width << 2))
-	    {
-	        mmby += 4;
-	        mmbx = 0;
-	    }
-	    if(mmby >= (clim_h->tile_height << 2))
-	    {
-	        mmby = 0;
-	        bx += (clim_h->tile_width << 2);
-	    }
-	    if(bx >= squared_width)
-	    {
-	        by += (clim_h->tile_height << 2);
-	        bx = 0;
-	    }
-	}
-	return out_data;
 }
 
 void clim_print(u8 *decomp_out, u32 decomp_size)
